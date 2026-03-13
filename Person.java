62e7f80c03536d60e091cbef96659df1ce446c62
Durdona <odiljonovad3@gmail.com>
2026-03-13 12:20:10 +0500

    Person

---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person {
+
+    String name;
+    String email;
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void role() {
+        System.out.println("Person uses the airline system.");
+    }
+}
